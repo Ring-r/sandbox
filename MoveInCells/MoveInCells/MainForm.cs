1160478fc71a3575298ddb356419fadd3f585f26
Ring-r <Ring_r@mail.ru>
2013-08-28 14:24:54 +0300

    Try to updete MoveInCells

--- MoveInCells/MoveInCells/MainForm.cs
+++ MoveInCells/MoveInCells/MainForm.cs
@@ -23,11 +23,14 @@ namespace MoveInCells
 
         private void MainForm_Paint(object sender, PaintEventArgs e)
         {
+            e.Graphics.Clear(Color.White);
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
             for (int i = 0; i < this.cells.GetLength(0); i++)
             {
                 for (int j = 0; j < this.cells.GetLength(1); j++)
                 {
-                    if (this.cells[i, j] != null)
+                    if (this.cells[i, j].Count > 0)
                     {
                         e.Graphics.FillRectangle(Brushes.Yellow, i << this.cellSize, j << this.cellSize, 1 << this.cellSize, 1 << this.cellSize);
                     }
@@ -42,6 +45,8 @@ namespace MoveInCells
 
                 e.Graphics.FillEllipse(entity.Brush, entity.X - entity.R, entity.Y - entity.R, 2 * entity.R, 2 * entity.R);
                 e.Graphics.DrawEllipse(Pens.Black, entity.X - entity.R, entity.Y - entity.R, 2 * entity.R, 2 * entity.R);
+                e.Graphics.DrawString(entity.T.ToString(), this.Font, Brushes.Black, entity.X + entity.R, entity.Y + entity.R);
+                e.Graphics.DrawLine(Pens.Black, entity.X, entity.Y, entity.X + entity.VX * entity.T, entity.Y + entity.VY * entity.T);
             }
         }
 
@@ -90,6 +95,7 @@ namespace MoveInCells
                 entity.i = (int)entity.X >> this.cellSize;
                 entity.j = (int)entity.Y >> this.cellSize;
                 this.cells[entity.i, entity.j].Add(entity);
+                this.RecalculateMinTime(entity);
             }
 
             this.heap.StartFill(this.entities.ToArray());
@@ -105,20 +111,18 @@ namespace MoveInCells
                     this.RecalculateMinTime(entity);
                     entity = this.heap.GetFirst();
                 }
+                this.step(entity);
             }
         }
 
         private void RecalculateMinTime(Entity entity)
         {
-            this.RecalculateMinTimeToBlocks(entity);
-            this.RecalculateMinTimeToBorders(entity);
+            this.RecalculateMinTimeToCellsBorders(entity);
 
-            // TODO: 1 is not correct and uniq. What I need to do? What I need to do if radiuses of entities is not equal?
             int imin = Math.Max(0, entity.i - 1);
             int imax = Math.Min(entity.i + 1, this.cells.GetLength(0) - 1);
             int jmin = Math.Max(0, entity.j - 1);
             int jmax = Math.Min(entity.j + 1, this.cells.GetLength(1) - 1);
-
             for (int i = imin; i <= imax; ++i)
             {
                 for (int j = jmin; j <= jmax; ++j)
@@ -134,14 +138,14 @@ namespace MoveInCells
             }
         }
 
-        private void RecalculateMinTimeToBlocks(Entity entity) // t_stenki(ic)
+        private void RecalculateMinTimeToCellsBorders(Entity entity)
         {
-            //throw new NotImplementedException();
-        }
-
-        private void RecalculateMinTimeToBorders(Entity entity) // t_gran_in_cell(ic)
-        {
-            //throw new NotImplementedException();
+            int ix = entity.VX < 0 ? entity.i << this.cellSize : (entity.i + 1) << this.cellSize;
+            float tx = entity.TLoc + (ix - entity.X) / entity.VX;
+            int jy = entity.VY < 0 ? entity.j << this.cellSize : (entity.j + 1) << this.cellSize;
+            float ty = entity.TLoc + (jy - entity.Y) / entity.VY;
+            entity.T = tx < ty ? tx : ty;
+            entity.Event = 1;
         }
 
         private void RecalculateMinTimeToEntity(Entity entity, Entity entity_2) // t_chast(ic,jc)
@@ -160,25 +164,43 @@ namespace MoveInCells
             float t1 = (float)(B - Math.Sqrt(D)) / A;
             float t2 = (float)(B + Math.Sqrt(D)) / A;
 
-            // procedure t_chast (ic, jc : longint);
-            //double test = 1e20;
-            //bool ind_sosed = false;
-            //bool ind_tip = entity.tip == 2 && entity_2.tip == 2;
-            //if (ind_tip)
-            //    if (entity.k_sosedej > 0)
-            //    {
-            //        int k = 1;
-            //        while (entity.sosedi[k] != entity_2 && k <= entity.k_sosedej)
-            //            ++k;
-            //        if (k <= entity.k_sosedej)
-            //            ind_sosed = true;
-            //    }
-            //if (ind_sosed)
-            //    t_sosedi(ic, jc, test);
-            //else
-            //    t_not_sosedi(ic, jc, test);
+            if (entity.T > t1)
+            {
+                entity.T = t1;
+            }
+            if (entity_2.T > t2)
+            {
+                entity_2.T = t2;
+            }
         }
 
+        // procedure t_chast (ic, jc : longint);
+        //double test = 1e20;
+        //bool ind_sosed = false;
+        //bool ind_tip = entity.tip == 2 && entity_2.tip == 2;
+        //if (ind_tip)
+        //    if (entity.k_sosedej > 0)
+        //    {
+        //        int k = 1;
+        //        while (entity.sosedi[k] != entity_2 && k <= entity.k_sosedej)
+        //            ++k;
+        //        if (k <= entity.k_sosedej)
+        //            ind_sosed = true;
+        //    }
+        //if (ind_sosed)
+        //    t_sosedi(ic, jc, test);
+        //else
+        //    t_not_sosedi(ic, jc, test);
+        //    new_times(ic, jc, test);
+
+        private void step(Entity entity)
+        {
+            switch (entity.Event)
+            {
+                case 0: this.step_0(entity); break;
+                case 1: this.step_1(entity); break;
+            }
+        }
 
         private void step_0(Entity entity)
         {
@@ -196,6 +218,19 @@ namespace MoveInCells
         }
 
         private void event_0(Entity entity, Entity entityNext)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void step_1(Entity entity)
+        {
+            entity.Move();
+            this.event_1(entity);
+            this.RecalculateMinTime(entity);
+            this.heap.Recalculate(entity.Id);
+        }
+
+        private void event_1(Entity entity)
         {
             throw new NotImplementedException();
         }
