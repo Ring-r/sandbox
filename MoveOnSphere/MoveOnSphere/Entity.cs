e49d2b4949a1c67e574f9cda2f1a13dff9306343
Ring-r <Ring_r@mail.ru>
2013-09-05 20:01:57 +0300

    Add MoveOnSphere. End work day.

---
+++ MoveOnSphere/MoveOnSphere/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Media.Media3D;
+
+namespace MoveOnSphere
+{
+    public class Entity
+    {
+        public Vector3D v, v_;
+        public float w, w_;
+        public float r;
+
+        public void Move()
+        {
+            Point3D p = new Point3D();
+
+            Quaternion q = new Quaternion(v, w);
+            Matrix3D m = new Matrix3D();
+            m.RotateAt(q, p);
+            v_ = m.Transform(v_);
+
+            Quaternion q_ = new Quaternion(v_, w_);
+            Matrix3D m_ = new Matrix3D();
+            m_.RotateAt(q_, p);
+            v = m_.Transform(v);
+
+        }
+    }
+}
---
+++ MoveOnSphere/MoveOnSphere/MainForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Windows.Media.Media3D;
+
+namespace MoveOnSphere
+{
+    public partial class MainForm : Form
+    {
+        private readonly Random random = new Random();
+        private readonly Entity entity = new Entity();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.timer.Interval = 100;
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            float r = Math.Min(this.ClientSize.Width, this.ClientSize.Height) / 2;
+
+            float x = this.random.Next((int)r / 2);
+            float y = this.random.Next((int)r / 2);
+            float z = (float)Math.Sqrt(r * r - x * x - y * y);
+
+            this.entity.r = r;
+
+            this.entity.v = new Vector3D(x, y, z);
+            this.entity.v.Normalize();
+            this.entity.w = 0;
+
+            this.entity.v_ = Vector3D.CrossProduct(this.entity.v, new Vector3D(0, 0, 1));
+            this.entity.v_.Normalize();
+            this.entity.w_ = 0;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.White);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            e.Graphics.TranslateTransform(this.ClientSize.Width / 2, this.ClientSize.Height / 2);
+
+            e.Graphics.DrawEllipse(Pens.Black, -this.entity.r, -this.entity.r, 2 * this.entity.r, 2 * this.entity.r);
+            float r = (float)(10 * (this.entity.v.Z + 1) / 2 + 10);
+            Brush brush = new SolidBrush(Color.FromArgb((int)(200 * (this.entity.v.Z + 1) / 2) + 55, Color.Blue));
+            e.Graphics.FillEllipse(brush, (float)(this.entity.v.X * this.entity.r) - r / 2, (float)(this.entity.v.Y * this.entity.r) - r / 2, r, r);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (this.random.Next(100) < 5)
+            {
+                this.entity.w = 10 * (float)this.random.NextDouble() - 5;
+            }
+            if (this.random.Next(100) >= 95)
+            {
+                this.entity.w_ = 10 * (float)this.random.NextDouble() - 5;
+            }
+            this.entity.Move();
+            this.Invalidate();
+        }
+    }
+}
