46ec9a6a5ea250cb783a934ed4e34a3e6ebb7124
Ring-r <Rostyslav.Pudlo@gmail.com>
2015-04-15 13:33:32 +0300

    Use World class. Random fill and draw world.

--- capton/capton.py
+++ capton/capton.py
@@ -4,17 +4,29 @@ import sys
 import os
 import pygame
 import time
+import world
 
 title = "capton"
 resolution = (600, 600)
 flag = 0 # pygame.FULLSCREEN
 depth = 32
-back_color = (0, 0, 0)
-
+color_back = (0, 0, 0)
 
 pygame.init()
 pygame.display.set_caption(title)
 screen = pygame.display.set_mode(resolution, flag, depth)
+
+i_count = 30
+j_count = 30
+fill_percent = 20
+cell_size = 20
+cell_border_size = 1
+color_empty = (12, 12, 12)
+color_block = (192, 192, 192)
+
+world = world.World()
+world.init(i_count, j_count)
+world.fill_random(fill_percent)
 
 while True:
 	for event in pygame.event.get():
@@ -26,7 +38,9 @@ while True:
 				pygame.quit()
 				os._exit(0) # sys.exit(0)
 
-	screen.fill(back_color)  
+	screen.fill(color_back)
+
+	world.draw(screen, color_empty, color_block, cell_size, cell_border_size)
 
 	pygame.display.update()
 
