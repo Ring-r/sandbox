0ce957e2d7215f6d225c4ed4f7024eab083aa38d
Ring-r <Rostyslav.Pudlo@gmail.com>
2015-07-16 22:29:39 +0300

    Change control.
    Using mouse for rotate map.

--- Knockout/Knockout.cs
+++ Knockout/Knockout.cs
@@ -50,9 +50,8 @@ namespace Knockout {
 		private readonly Stopwatch stopwatch = new Stopwatch();
 
 		private readonly int bonusCount = 10;
-		private readonly int blockCount = 10;
-		private readonly int controlCount = 3;
-		private readonly int controlIndex = 1;
+		private readonly int blockCount = 100;
+		private readonly int controlCount = 2;
 
 		private readonly int ballRadius = 10;
 		private readonly int bonusRadius = 10;
@@ -60,11 +59,6 @@ namespace Knockout {
 		private readonly int controlRadius = 10;
 
 		private readonly float ballSpeed = 0.1f;
-
-		private readonly float xMin = 1.0f / 4;
-		private readonly float xMax = 3.0f / 4;
-		private readonly float yMin = 1.0f / 4;
-		private readonly float yMax = 3.0f / 4;
 
 		private readonly EntityCircle ball = new EntityCircle();
 		private readonly List<EntityCircle> controlList = new List<EntityCircle>();
@@ -76,6 +70,9 @@ namespace Knockout {
 		private readonly Brush bonusBrush = Brushes.Yellow;
 		private readonly Brush blockBrush = Brushes.Red;
 		private readonly Brush controlBrush = Brushes.Silver;
+
+		private int x = 0;
+		private int y = 0;
 
 		private bool isPause = true;
 		private bool isShowInfo = true;
@@ -97,19 +94,29 @@ namespace Knockout {
 			}
 		}
 
+		private void ChangeControlList() {
+			this.controlList[0].px = this.controlList[this.controlList.Count - 1].px;
+			this.controlList[0].py = this.controlList[this.controlList.Count - 1].py;
+			for (int i = 1; i < this.controlCount; ++i) {
+				var control = this.controlList[i];
+				control.px = (float)Program.rand.NextDouble();
+				control.py = (float)Program.rand.NextDouble();
+			}
+		}
+
 		private void CreateLevel() {
 			this.ball.brush = this.ballBrush;
 			this.ball.radius = this.ballRadius;
 			this.ball.speed = this.ballSpeed;
-			this.ball.time = 0.0f;
+			this.ball.alfa = 0.0f;
 
 			this.objectList.Clear();
 			for (int i = 0; i < this.bonusCount; ++i) {
 				this.objectList.Add(new EntityCircle() {
 					brush = this.bonusBrush,
 					radius = this.bonusRadius,
-					px = (float)Program.rand.NextDouble() * (this.xMax - this.xMin) + this.xMin,
-					py = (float)Program.rand.NextDouble() * (this.yMax - this.yMin) + this.yMin,
+					px = (float)Program.rand.NextDouble(),
+					py = (float)Program.rand.NextDouble(),
 				});
 			}
 
@@ -117,8 +124,8 @@ namespace Knockout {
 				this.objectList.Add(new EntityCircle() {
 					brush = this.blockBrush,
 					radius = this.blockRadius,
-					px = (float)Program.rand.NextDouble() * (this.xMax - this.xMin) + this.xMin,
-					py = (float)Program.rand.NextDouble() * (this.yMax - this.yMin) + this.yMin,
+					px = (float)Program.rand.NextDouble(),
+					py = (float)Program.rand.NextDouble(),
 				});
 			}
 
@@ -139,7 +146,7 @@ namespace Knockout {
 		}
 
 		private void StartLevel() {
-			this.ball.time = 0;
+			this.ball.alfa = 0;
 			this.ball.px = this.controlList[0].px;
 			this.ball.py = this.controlList[0].py;
 		
@@ -150,13 +157,22 @@ namespace Knockout {
 			e.Graphics.Clear(this.backgroundColor);
 			e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
 
-			this.ball.Draw(e.Graphics, this.ClientSize.Width, this.ClientSize.Height);
+			float x = this.x - this.ClientSize.Width / 2;
+			float y = this.y - this.ClientSize.Height / 2;
+			float angle = (float)Math.Atan2(y, x);
+
+			float scale = Math.Min(this.ClientSize.Width, this.ClientSize.Height);
+
+			e.Graphics.TranslateTransform(-scale / 2, -scale / 2);
+			e.Graphics.TranslateTransform(this.ClientSize.Width / 2, this.ClientSize.Height / 2);
+
+			this.ball.Draw(e.Graphics, scale);
+			foreach (var control in this.controlList) {
+				control.Draw(e.Graphics, scale);
+			}
 
 			foreach (var circle in this.objectList) {
-				circle.Draw(e.Graphics, this.ClientSize.Width, this.ClientSize.Height);
-			}
-			foreach (var circle in this.controlList) {
-				circle.Draw(e.Graphics, this.ClientSize.Width, this.ClientSize.Height);
+				circle.Draw(e.Graphics, scale, angle);
 			}
 		}
 
@@ -164,35 +180,36 @@ namespace Knockout {
 			float timeEllapsed = 0.001f * this.stopwatch.ElapsedMilliseconds;
 			this.stopwatch.Restart();
 
-			if (!this.isPause) {
-				if (this.ball.time < 1f) {
-					this.ball.time += this.ball.speed * timeEllapsed;
+			if (this.isPause) {
+				return;
+			}
 
-					List<PointF> points = this.controlList.Select(item => new PointF(item.px, item.py)).ToList();
-					while (points.Count > 1) {
-						for (int i = 0; i < points.Count - 1; ++i) {
-							points[i] = new PointF(
-								points[i].X * (1 - this.ball.time) + points[i + 1].X * this.ball.time,
-								points[i].Y * (1 - this.ball.time) + points[i + 1].Y * this.ball.time);
-						}
-						points.RemoveAt(points.Count - 1);
-					}
-					this.ball.px = points[0].X;
-					this.ball.py = points[0].Y;
-				} else {
-					this.ball.px = -100;
-					this.ball.py = -100;
+			this.ball.alfa += this.ball.speed * timeEllapsed;
+
+			if (this.ball.alfa > 1.0f) {
+				this.ChangeControlList();
+				this.ball.alfa -= 1.0f;
+			}
+
+			List<PointF> points = this.controlList.Select(item => new PointF(item.px, item.py)).ToList();
+			for (int j = 0; j < points.Count - 1; ++j) {
+				for (int i = 0; i < points.Count - 1; ++i) {
+					points[i] = new PointF(
+						points[i].X * (1 - this.ball.alfa) + points[i + 1].X * this.ball.alfa,
+						points[i].Y * (1 - this.ball.alfa) + points[i + 1].Y * this.ball.alfa);
 				}
 			}
+			this.ball.px = points[0].X;
+			this.ball.py = points[0].Y;
+
 			this.Invalidate();
 		}
 
 		private void MainForm_MouseMove(object sender, MouseEventArgs e) {
-			float controlBegin = this.ClientSize.Width / 4;
-			float controlLength = this.ClientSize.Width / 2;
-			if (controlBegin < e.X && e.X < controlBegin + controlLength) {
-				this.controlList[this.controlIndex].py = (e.X - controlBegin) / controlLength;
-			}
+			this.x = e.X;
+			this.y = e.Y;
+
+			this.Invalidate();
 		}
 	}
 
@@ -204,13 +221,19 @@ namespace Knockout {
 		public float px = 0;
 		public float py = 0;
 
-		public void Draw(Graphics g, float xScale, float yScale) {
-			g.FillEllipse(this.brush, xScale * this.px - this.radius, yScale * this.py - this.radius, 2 * this.radius, 2 * this.radius);
+		public void Draw(Graphics g, float scale, float angle = 0.0f) {
+			float x = this.px - 0.5f;
+			float y = this.py - 0.5f;
+			float xx = x * (float)Math.Cos(angle) - y * (float)Math.Sin(angle);
+			float yy = x * (float)Math.Sin(angle) + y * (float)Math.Cos(angle);
+			x = xx + 0.5f;
+			y = yy + 0.5f;
+			g.FillEllipse(this.brush, scale * x - this.radius, scale * y - this.radius, 2 * this.radius, 2 * this.radius);
 		}
 	}
 
 	class EntityCircle : Circle {
-		public float speed = 0;
-		public float time = 0;
+		public float alfa = 0.0f;
+		public float speed = 0.0f;
 	}
 }
