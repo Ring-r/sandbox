d1e5f7e53cf8c89039437d8dcdedac2e0b11b395
Anna <Anna@CREATURE>
2016-06-19 21:50:31 +0300

    Move Forms logic to library.

---
+++ Prototypes/Forms/FormWithYieldTimer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prototypes.Forms
+{
+	public abstract class FormWithYieldTimer : FormWithTimer
+	{
+		protected IEnumerator<Boolean> enumerator = null;
+
+		protected void DisposeEnumerator()
+		{
+			if (enumerator != null)
+			{
+				this.enumerator.Dispose();
+				this.enumerator = null;
+			}
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			this.DisposeEnumerator();
+			base.Dispose(disposing);
+		}
+
+		protected override void Timer_Tick(object sender, EventArgs e)
+		{
+			if (this.enumerator != null && this.enumerator.MoveNext())
+			{
+				this.Invalidate();
+			}
+		}
+	}
+}
