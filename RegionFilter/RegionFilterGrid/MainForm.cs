0349d7cb2c495a322d4bf713b65122a704812c31
Rostyslav Pudlo <rostyslav.pudlo@gmail.com>
2017-09-27 17:25:14 +0300

    Refactoring. Rename projects

---
+++ RegionFilter/RegionFilterGrid/MainForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+using AlgorithmsAndDataStructures;
+
+namespace RegionFilter
+{
+    public partial class MainForm : Form
+    {
+        #region Private fields.
+
+        private readonly RegionBuilder regionBuilder;
+
+        private long calculationTime;
+
+        private bool isDrawStrings = true;
+        private readonly Brush stringsBrush = Brushes.Yellow;
+
+
+        private readonly GridWithGeometry<bool> grid = new GridWithGeometry<bool>(0, 0);
+        private readonly GridWithGeometryViewer gridWithGeometryViewer;
+
+        #endregion Private fields.
+
+        private void Calculate()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            this.grid.ReInitialize((int)(this.ClientSize.Width / this.grid.IStepSize) + 1, (int)(this.ClientSize.Height / this.grid.JStepSize) + 1);
+            this.grid.Border(this.regionBuilder.Data);
+
+            var startIndex = 0;
+            while (startIndex < this.grid.CellsCount && !Convert.ToBoolean(this.grid[startIndex]))
+            {
+                ++startIndex;
+            }
+            if (startIndex != this.grid.CellsCount)
+            {
+                var gridVector = this.grid.Border(startIndex);
+                this.grid.SetValue(true, gridVector);
+            }
+
+            stopwatch.Stop();
+            this.calculationTime = stopwatch.ElapsedMilliseconds;
+        }
+
+        private void DrawStrings(Graphics graphics)
+        {
+            var textY = 0f;
+            graphics.DrawString("F1 - help", this.Font, this.stringsBrush, 0.0f, textY);
+
+            if (!this.isDrawStrings)
+            {
+                return;
+            }
+
+            var textHeight = graphics.MeasureString(" ", this.Font).Height;
+
+            textY += textHeight; graphics.DrawString("F2 - show/hide contour", this.Font, this.stringsBrush, 0.0f, textY);
+            textY += textHeight; graphics.DrawString("F5 - clean", this.Font, this.stringsBrush, 0.0f, textY);
+            textY += textHeight;
+            textY += textHeight; graphics.DrawString("To add point (points count in current part less then 3) click mouse left button in any place.", this.Font, this.stringsBrush, 0.0f, textY);
+            textY += textHeight; graphics.DrawString("To add point (points count in current part more then 3) click mouse left button on edge.", this.Font, this.stringsBrush, 0.0f, textY);
+            textY += textHeight; graphics.DrawString("To move point click mouse left button on point, hold and move.", this.Font, this.stringsBrush, 0.0f, textY);
+            textY += textHeight; graphics.DrawString("To delete point click mouse right button on point.", this.Font, this.stringsBrush, 0.0f, textY);
+            textY += textHeight; graphics.DrawString("To delete part delete all points in part.", this.Font, this.stringsBrush, 0.0f, textY);
+            textY += textHeight;
+            textY += textHeight; graphics.DrawString($"Count of region points: {this.regionBuilder.Data.Sum(part => part.Count)}", this.Font, this.stringsBrush, 0.0f, textY);
+            textY += textHeight;
+            textY += textHeight; graphics.DrawString($"Calculation time: {this.calculationTime} milliseconds", this.Font, this.stringsBrush, 0.0f, textY);
+            textY += textHeight;
+            textY += textHeight; graphics.DrawString("Exc - close", this.Font, this.stringsBrush, 0.0f, textY);
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            this.grid.ReInitialize((int)(this.ClientSize.Width / this.grid.IStepSize) + 1, (int)(this.ClientSize.Height / this.grid.JStepSize) + 1);
+
+            this.Calculate();
+            this.Invalidate();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(this.BackColor);
+
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            this.gridWithGeometryViewer.Draw(this.grid, e.Graphics, this.ClientSize);
+
+            this.regionBuilder.Draw(e.Graphics);
+
+            this.DrawStrings(e.Graphics);
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Escape)
+            {
+                this.Close();
+            }
+            if (e.KeyData == Keys.F1)
+            {
+                this.isDrawStrings = !this.isDrawStrings;
+
+                this.Invalidate();
+            }
+            if (e.KeyData == Keys.F2)
+            {
+                this.regionBuilder.IsDraw = !this.regionBuilder.IsDraw;
+
+                this.Invalidate();
+            }
+            if (e.KeyData == Keys.F5)
+            {
+                this.regionBuilder.Clear();
+            }
+        }
+
+        private void RegionBuilderChanged(object sender, EventArgs e)
+        {
+            this.Calculate();
+            this.Invalidate();
+        }
+
+        private void RegionBuilderChangedLight(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        public MainForm()
+        {
+            this.regionBuilder = new RegionBuilder(this)
+            {
+                PointSize = 20.0f,
+                IsDraw = true,
+                Color = Color.Red,
+                PartAlpha = 100,
+                PartWidth = 1.0f,
+                CurrentPartAlpha = 255,
+                CurrentPartWidth = 2.0f,
+            };
+            this.regionBuilder.Changed += this.RegionBuilderChanged;
+            this.regionBuilder.ChangedLight += this.RegionBuilderChangedLight;
+
+            this.grid.IStepSize = 15.2;
+            this.grid.JStepSize = 14.8;
+
+            this.gridWithGeometryViewer = new GridWithGeometryViewer
+            {
+                Pen = Pens.Gray,
+                Brush = Brushes.Gray,
+            };
+
+            this.InitializeComponent();
+        }
+    }
+}
+
---
+++ RegionFilter/RegionFilterGrid/RegionBuilder.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Media.Media3D;
+
+namespace RegionFilter
+{
+    public sealed class RegionBuilder
+    {
+        private int indexPart = -1;
+        private int indexLine = -1;
+        private int indexPoint = -1;
+
+        private Point mousePoint;
+        private bool updateOnMouseUp;
+
+        public List<List<Point3D>> Data { get; } = new List<List<Point3D>>();
+
+        public float PointSize { get; set; }
+
+        public bool IsDraw { get; set; }
+        public Color Color { get; set; }
+        public int PartAlpha { get; set; }
+        public float PartWidth { get; set; }
+        public int CurrentPartAlpha { get; set; }
+        public float CurrentPartWidth { get; set; }
+
+        public event EventHandler Changed;
+        public event EventHandler ChangedLight;
+
+        public RegionBuilder(Form form)
+        {
+            form.MouseDown += this.Form_MouseDown;
+            form.MouseMove += this.Form_MouseMove;
+            form.MouseUp += this.Form_MouseUp;
+        }
+
+        public void Clear()
+        {
+            this.Data.Clear();
+            this.indexPart = -1;
+            this.UpdateCurrentIndexes();
+
+            this.RaiseChanged();
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            if (!this.IsDraw)
+            {
+                return;
+            }
+
+            for (var j = 0; j < this.Data.Count; ++j)
+            {
+                var pen = new Pen(Color.FromArgb(j == this.indexPart ? this.CurrentPartAlpha : this.PartAlpha, this.Color));
+
+                var regionPart = this.Data[j];
+                for (var i = 0; i < regionPart.Count; ++i)
+                {
+                    pen.Width = j == this.indexPart && i == this.indexPoint ? this.CurrentPartWidth : this.PartWidth;
+                    var regionPoint = regionPart[i];
+                    graphics.DrawRectangle(pen, (float)(regionPoint.X) - this.PointSize / 2, (float)(regionPoint.Y) - this.PointSize / 2, this.PointSize, this.PointSize);
+
+                    pen.Width = j == this.indexPart && i == this.indexLine ? this.CurrentPartWidth : this.PartWidth;
+                    var regionPointSource = regionPart[i];
+                    var regionPointTarget = regionPart[i != regionPart.Count - 1 ? i + 1 : 0];
+                    graphics.DrawLine(pen, (float)(regionPointSource.X), (float)(regionPointSource.Y), (float)(regionPointTarget.X), (float)(regionPointTarget.Y));
+                }
+            }
+        }
+
+        private void RaiseChanged()
+        {
+            this.Changed?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void RaiseChangedLight()
+        {
+            this.ChangedLight?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void Form_MouseDown(object sender, MouseEventArgs e)
+        {
+            var isInvalidate = false;
+
+            if (e.Button == MouseButtons.Left && this.indexPoint < 0)
+            {
+                isInvalidate = this.AddPoint(e.X, e.Y);
+            }
+            if (e.Button == MouseButtons.Right && this.indexPoint >= 0)
+            {
+                if (this.DelPoint())
+                {
+                    if (this.Data[this.indexPart].Count == 0)
+                    {
+                        this.Data.RemoveAt(this.indexPart);
+                    }
+                    isInvalidate = true;
+                }
+            }
+
+            if (isInvalidate)
+            {
+                this.UpdateCurrentIndexes();
+                this.RaiseChanged();
+            }
+        }
+
+        private void Form_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.mousePoint = e.Location;
+
+            if (this.Data.Count == 0)
+            {
+                return;
+            }
+
+            var isInvalidate = false;
+
+            if (e.Button == MouseButtons.None)
+            {
+                isInvalidate = this.UpdateCurrentIndexes();
+            }
+
+            if (e.Button == MouseButtons.Left && this.indexPoint >= 0)
+            {
+                var regionPart = this.Data[this.indexPart];
+                regionPart[this.indexPoint] = new Point3D(e.X, e.Y, 0.0);
+                this.updateOnMouseUp = true;
+                isInvalidate = true;
+            }
+
+            if (isInvalidate)
+            {
+                this.RaiseChangedLight();
+            }
+        }
+
+        private void Form_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (this.updateOnMouseUp)
+            {
+                this.updateOnMouseUp = false;
+                this.RaiseChanged();
+            }
+        }
+
+        private bool AddPoint(double x, double y)
+        {
+            if (this.indexPart < 0)
+            {
+                var regionPart = new List<Point3D> { new Point3D(x, y, 0.0) };
+                this.Data.Add(regionPart);
+                this.indexPart = 0;
+            }
+            else
+            {
+                var regionPart = this.Data[this.indexPart];
+                if (regionPart.Count < 3)
+                {
+                    regionPart.Add(new Point3D(x, y, 0.0));
+                }
+                else
+                {
+                    if (this.indexLine >= 0)
+                    {
+                        regionPart.Insert(this.indexLine + 1, new Point3D(x, y, 0.0));
+                    }
+                    else
+                    {
+                        regionPart = this.Data[this.Data.Count - 1];
+                        if (regionPart.Count >= 3)
+                        {
+                            regionPart = new List<Point3D>();
+                        }
+                        regionPart.Add(new Point3D(x, y, 0.0));
+                        this.Data.Add(regionPart);
+                        this.indexPart = this.Data.Count - 1;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool DelPoint()
+        {
+            if (this.indexPart < 0)
+            {
+                return false;
+            }
+            if (this.indexPoint < 0)
+            {
+                return false;
+            }
+
+            var regionPart = this.Data[this.indexPart];
+            regionPart.RemoveAt(this.indexPoint);
+            return true;
+        }
+
+        private bool UpdateCurrentIndexes()
+        {
+            var currentPointIndex = this.FindCurrentPointIndex();
+            if (currentPointIndex.Item2 >= 0)
+            {
+                if (this.indexPart != currentPointIndex.Item1 || this.indexPoint != currentPointIndex.Item2)
+                {
+                    this.indexPart = currentPointIndex.Item1;
+                    this.indexPoint = currentPointIndex.Item2;
+                    this.indexLine = -1;
+                    return true;
+                }
+                return false;
+            }
+
+            var currentLineIndex = this.FindCurrentLineIndex();
+            if (currentLineIndex.Item2 >= 0)
+            {
+                if (this.indexPart != currentLineIndex.Item1 || this.indexLine != currentLineIndex.Item2)
+                {
+                    this.indexPart = currentLineIndex.Item1;
+                    this.indexLine = currentLineIndex.Item2;
+                    this.indexPoint = -1;
+                    return true;
+                }
+                return false;
+            }
+
+            if (this.indexPoint != -1 || this.indexLine != -1)
+            {
+                this.indexPoint = -1;
+                this.indexLine = -1;
+            }
+
+            return true;
+        }
+
+        private Tuple<int, int> FindCurrentPointIndex()
+        {
+            var resultIndexPart = -1;
+            var resultIndexPoint = -1;
+
+            if (0 < this.indexPart && this.indexPart < this.Data.Count)
+            {
+                var i = this.indexPart;
+                resultIndexPoint = this.FindCurrentPointIndex(i);
+                if (resultIndexPoint >= 0)
+                {
+                    resultIndexPart = i;
+                }
+            }
+
+            for (var i = 0; i < this.Data.Count && resultIndexPoint < 0; ++i)
+            {
+                resultIndexPoint = this.FindCurrentPointIndex(i);
+                if (resultIndexPoint >= 0)
+                {
+                    resultIndexPart = i;
+                }
+            }
+
+            return Tuple.Create(resultIndexPart, resultIndexPoint);
+        }
+
+        private int FindCurrentPointIndex(int inputIndexPart)
+        {
+            var resultIndexPoint = -1;
+            var regionPart = this.Data[inputIndexPart];
+            for (var i = 0; i < regionPart.Count; ++i)
+            {
+                var regionPoint = regionPart[i];
+                if (Math.Abs(this.mousePoint.X - regionPoint.X) <= this.PointSize / 2 && Math.Abs(this.mousePoint.Y - regionPoint.Y) <= this.PointSize / 2)
+                {
+                    resultIndexPoint = i;
+                    break;
+                }
+            }
+            return resultIndexPoint;
+        }
+
+        private Tuple<int, int> FindCurrentLineIndex()
+        {
+            var resultIndexPart = -1;
+            var resultIndexLine = -1;
+
+            if (0 < this.indexPart && this.indexPart < this.Data.Count)
+            {
+                var i = this.indexPart;
+                resultIndexLine = this.FindCurrentLineIndex(i);
+                if (resultIndexLine >= 0)
+                {
+                    resultIndexPart = i;
+                }
+            }
+
+            for (var i = 0; i < this.Data.Count && resultIndexLine < 0; ++i)
+            {
+                resultIndexLine = this.FindCurrentLineIndex(i);
+                if (resultIndexLine >= 0)
+                {
+                    resultIndexPart = i;
+                }
+            }
+
+            return Tuple.Create(resultIndexPart, resultIndexLine);
+        }
+
+        private int FindCurrentLineIndex(int inputIndexPart)
+        {
+            var resultIndexLine = -1;
+            var regionPart = this.Data[inputIndexPart];
+            for (var i = 0; i < regionPart.Count; ++i)
+            {
+                var regionPointSource = regionPart[i];
+                var regionPointTarget = regionPart[i != regionPart.Count - 1 ? i + 1 : 0];
+                if (
+                    Math.Abs(this.mousePoint.X - (regionPointSource.X + regionPointTarget.X) / 2) < Math.Abs(regionPointSource.X - regionPointTarget.X) / 2 &&
+                    Math.Abs(this.mousePoint.Y - (regionPointSource.Y + regionPointTarget.Y) / 2) < Math.Abs(regionPointSource.Y - regionPointTarget.Y) / 2)
+                {
+                    var vx = regionPointTarget.X - regionPointSource.X;
+                    var vy = regionPointTarget.Y - regionPointSource.Y;
+                    var d = Math.Sqrt(vx * vx + vy * vy);
+                    if (d != 0)
+                    {
+                        vx /= d;
+                        vy /= d;
+                        if (Math.Abs((this.mousePoint.X - regionPointSource.X) * vy - (this.mousePoint.Y - regionPointSource.Y) * vx) <= this.PointSize / 2)
+                        {
+
+                            resultIndexLine = i;
+                            break;
+                        }
+                    }
+                }
+            }
+            return resultIndexLine;
+        }
+    }
+}
